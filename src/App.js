a4d8bcba79c08405133b256bdea7b8fee1889b24
Mukul Sharma <185355609+MukulSha-afk@users.noreply.github.com>
2026-08-31 15:06:03 +0530

    Refactor routes and add missing components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,62 @@
 import "./App.css";
-import {Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"
-import Navbar from "./components/common/Navbar"
-import OpenRoute from "./components/core/Auth/OpenRoute"
 
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import Home from "./pages/Home";
+import Navbar from "./components/common/Navbar";
+import OpenRoute from "./components/core/Auth/OpenRoute";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Settings from "./components/core/Dashboard/Settings";
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
+import Cart from "./components/core/Dashboard/Cart";
+
+import MyCourses from "./components/core/Dashboard/MyCourses";
+import AddCourse from "./components/core/Dashboard/AddCourse";
+import EditCourse from "./components/core/Dashboard/EditCourse";
+
+import { ACCOUNT_TYPE } from "./utils/constants";
 
 function App() {
+
+  const { user } = useSelector((state) => state.profile);
+
   return (
-   <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route
-          path="signup"
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+
+      <Navbar />
+
+      <Routes>
+
+        {/* ================= PUBLIC ROUTES ================= */}
+
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/signup"
           element={
             <OpenRoute>
               <Signup />
             </OpenRoute>
           }
         />
-    <Route
-          path="login"
+
+        <Route
+          path="/login"
           element={
             <OpenRoute>
               <Login />
@@ -34,46 +64,115 @@ function App() {
           }
         />
 
-    <Route
-          path="forgot-password"
+        <Route
+          path="/forgot-password"
           element={
             <OpenRoute>
               <ForgotPassword />
             </OpenRoute>
           }
-        />  
+        />
 
-      <Route
-          path="verify-email"
+        <Route
+          path="/verify-email"
           element={
             <OpenRoute>
               <VerifyEmail />
             </OpenRoute>
           }
-        />  
+        />
 
-    <Route
-          path="update-password/:id"
+        <Route
+          path="/update-password/:id"
           element={
             <OpenRoute>
               <UpdatePassword />
             </OpenRoute>
           }
-        />  
+        />
 
-    <Route
-          path="about"
+        <Route path="/about" element={<About />} />
+
+        <Route path="/contact" element={<Contact />} />
+
+
+        {/* ================= PRIVATE DASHBOARD ================= */}
+
+        <Route
           element={
-            <OpenRoute>
-              <About />
-            </OpenRoute>
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
           }
-        />  
+        >
+
+          {/* PROFILE */}
+
+          <Route
+            path="/dashboard/my-profile"
+            element={<MyProfile />}
+          />
 
 
-    </Routes>
+          {/* SETTINGS */}
 
-   </div>
+          <Route
+            path="/dashboard/settings"
+            element={<Settings />}
+          />
+
+
+          {/* ================= STUDENT ================= */}
+
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route
+                path="/dashboard/cart"
+                element={<Cart />}
+              />
+
+              <Route
+                path="/dashboard/enrolled-courses"
+                element={<EnrolledCourses />}
+              />
+            </>
+          )}
+
+
+          {/* ================= INSTRUCTOR ================= */}
+
+          {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+            <>
+              <Route
+                path="/dashboard/my-courses"
+                element={<MyCourses />}
+              />
+
+              <Route
+                path="/dashboard/add-course"
+                element={<AddCourse />}
+              />
+
+              <Route
+                path="/dashboard/edit-course/:courseId"
+                element={<EditCourse />}
+              />
+            </>
+          )}
+
+        </Route>
+
+
+        {/* ================= 404 ================= */}
+
+        <Route
+          path="*"
+          element={<Error />}
+        />
+
+      </Routes>
+
+    </div>
   );
 }
 
